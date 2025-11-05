72ac112f91a6fd77c94eed8b7a767e245f85e5a8
1lelanta <kekikoki4@gmail.com>
2025-11-05 21:09:28 +0300

    fix response object in get user listing api

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -77,9 +77,10 @@ export const getUser = async (req,res,next)=>{
   if(!user) return next(errorHandler(404,"User not found"));
 
   const {password:pass, ...rest} = user._doc;
+   res.status(200).json(rest);
   } catch (error) {
     next(error);
   }
 
-  res.status(200).json(rest);
+ 
 }
