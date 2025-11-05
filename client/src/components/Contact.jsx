841bdc903abf8e4caad8b734c8fedc21d84351e4
1lelanta <kekikoki4@gmail.com>
2025-11-05 21:31:35 +0300

    add estate  owner contact page

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Contact({listing}){
+  const [landlord, setLandlord] = useState(null);
+
+  useEffect(()=>{
+    const fetchLandlord= async ()=>{
+       try {
+      const res =  await fetch(`/api/user/${listing.userRef}`);
+      const data = await res.json();
+      setLandlord(data)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  fetchLandlord();
+    
+  },[listing.userRef])
+
+   
   return(
-    <div></div>
-  )
-}
+    <>
+    {landlord &&(
+      <div className=''>
+      <p className='font-semibold'>Contact <span>{landlord.username}</span> for <span className='font-semibold'>{listing.name.toLowerCase()}</span></p>
+    </div>
+    )}
+    
+    </>
+  )}
