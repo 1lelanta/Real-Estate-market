ea2cc1ec8c6eb4f25177e6e7b134e00a3f1ac8ad
1lelanta <kekikoki4@gmail.com>
2025-11-06 18:43:41 +0300

    add contact landlord functionality to the listing

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from 'react'
+import {Link} from 'react-redux';
 
 export default function Contact({listing}){
   const [landlord, setLandlord] = useState(null);
+  const [message, setMessage] = useState("");
+  const onchange = (e)=>{
+    setMessage(e.target.value);
+  }
 
   useEffect(()=>{
     const fetchLandlord= async ()=>{
@@ -21,8 +26,17 @@ export default function Contact({listing}){
   return(
     <>
     {landlord &&(
-      <div className=''>
-      <p className='font-semibold'>Contact <span>{landlord.username}</span> for <span className='font-semibold'>{listing.name.toLowerCase()}</span></p>
+      <div className='flex flex-col gap-2'>
+      <p className='font-semibold'>Contact <span>{landlord.username}</span> 
+      for <span className='font-semibold'>{listing.name.toLowerCase()}</span></p>
+      <textarea value={message} name='message' id='message' rows="2"
+      onChange={onchange} placeholder='message the owner'
+      className='w-full border p-3 rounded-lg'></textarea>
+      <Link to={`mailto:${landlord.email}?subject=Regarding
+        ${listing.name}&body=${message}`}
+        className="bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95">
+      Send Message
+      </Link>
     </div>
     )}
     
