03ea8686e4c6cc11fcb90c6bdfaf49f6873b9820
1lelanta <kekikoki4@gmail.com>
2025-11-07 19:12:44 +0300

    Create search page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing'
 import UpdateListing from './pages/UpdateListing'
 import Listing from './pages/Listing'
+import { Search } from './pages/Search'
 
 const App = () => {
   return (
@@ -20,9 +21,10 @@ const App = () => {
       <Route path="/sign-in" element={<Signin/>}/>
       <Route path="/sign-up" element={<SignUp/>}/>
       <Route path='/about' element={<About/>}/>
+      <Route path='/search' element={<Search/>}/>
       <Route path='/listing/:listingId' element={<Listing/>}/>
       <Route element={<PrivateRoute/>}>
-        <Route path="/profile" element={<Profile/>}/>
+      <Route path="/profile" element={<Profile/>}/>
   
       <Route path="/create-listing" element={<CreateListing/>}/>
       <Route path="/update-listing/:listingId" element={<UpdateListing/>}/>
