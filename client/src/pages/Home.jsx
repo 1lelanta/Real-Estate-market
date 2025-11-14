55595b6787689c501e20bd85568cbc87ec9407f7
1lelanta <kekikoki4@gmail.com>
2025-11-14 20:41:03 +0300

    add offering list for homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useEffect, useReducer, useState } from 'react'
+import { Link } from 'react-router-dom'
 
-const Home = () => {
+export default function Home() {
+  const [offerListing, setOfferListing] = useState([]);
+  const [saleListings, setSaleListings] = useState([]);
+  const [rentListings, setRentListings] = useState([]);
+
+
+  useEffect(()=>{
+    const fetchOfferListings = async()=>{
+      try {
+        const res = await fetch('/api/listing/get?offer=true&limit=4');
+        const data = res.json();
+
+        setOfferListing(data);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchOfferListings();
+
+  },[]);
+
   return (
-    <div>Home</div>
+    <div>
+      {/* top */}
+      <div className='flex flex-col gap-6 p-28 px-3 max-w-3x 
+      mx-auto'>
+        <h1 className='text-slate-700 font-bold text-3xl
+        lg:text-6xl'>
+          Find your next <span className='text-slate-500'>perfect</span>
+          <br/>
+           place with ease
+        </h1>
+        <div className='text-gray-400 text-xs sm:text-sm'>
+          lelanta Estate market is the best place to Find
+           your next home to live.
+           <br/>
+
+           We have a wide range of properties for YOU to
+           choose from
+
+        </div>
+          <Link to={"/search"} className='text-xs sm:text-sm 
+          text-blue-800 font-bold hover:underline'>
+          let's get started
+          </Link>
+      </div>
+
+      {/* swiper */}
+
+      {/* listing results for offer, sale and rent */}
+    </div>
   )
 }
-
-export default Home
