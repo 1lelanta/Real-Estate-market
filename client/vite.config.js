b8a950aea61500dfa1282f1e1ed7bee766c7727d
!lelanta <kekikoki4@gmail.com>
2026-01-26 15:22:11 +0300

    fix vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,14 +2,5 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://real-estate-market-tggk.onrender.com',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
   plugins: [tailwindcss()],
 })
