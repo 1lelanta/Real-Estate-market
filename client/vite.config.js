eff86d839124a1f1d671d03f1621b89c186102db
!lelanta <kekikoki4@gmail.com>
2026-01-26 13:57:09 +0300

    add backend deployement url in config

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:300 https://real-estate-market-tggk.onrender.com',
         changeOrigin: true,
         secure: false,
       },
